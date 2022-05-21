1ff02f97e08f7b7333e08ce2e55fc6700097bf71
Keeeparis <vladimir1trotsenko@gmail.com>
2022-05-21 21:04:54 +0600

    added marks

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -7,7 +7,7 @@ interface Props {
 export class Error extends PureComponent<Props, unknown> {
   render() {
     const { msg } = this.props
-    return <div>{msg}. Try refreshing the page!</div>
+    return <div>{msg}. Try refreshing the page!!!</div>
   }
 }
 
